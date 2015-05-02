ea4fd47c83bdbbc018e782f51e347b693dfc0ac9
Jeremy Goecks <jgoecks@gwu.edu>
2015-05-01 21:57:35 -0400

    Update GEMINI installation script.

--- install/install_gemini.sh
+++ install/install_gemini.sh
@@ -1,21 +1,12 @@
 #
-# Install GEMINI and SnpEff
+# Install GEMINI (https://github.com/arq5x/gemini) into ./gemini
 # NOTE: GEMINI creates symbolic links using full paths, so it's difficult to move GEMINI once it's created.
 #
 
-# Parameter checking.
-if [ $# -ne "1" ]
-then
-  echo "Usage: `basename $0` <install_dir>"
-  exit -1
-fi
-
-pushd $1
-
-# -- Install GEMINI --
-
 mkdir gemini
 pushd gemini
+
+# -- Install GEMINI --
 
 # Get GEMINI installation script.
 wget https://raw.github.com/arq5x/gemini/master/gemini/scripts/gemini_install.py
@@ -25,11 +16,9 @@ python gemini_install.py . . --nosudo
 
 # Clean up.
 rm gemini_install.py
-
-popd
 popd
 
-# -- Install SnpEff and human database. --
-
-brew install snpeff
-snpEff download -v GRCh37.75
+# Add symbolic link to /bin directory if it exists.
+if [ -d "bin" ]; then
+  pushd bin && ln -s ../gemini/gemini .
+fi
